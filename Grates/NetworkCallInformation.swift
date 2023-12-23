a96748695d8b06ab67d2cb0dfe66ffab94c2616c
Pavel Maslov <pavel_maslov_05@mail.ru>
2023-12-23 03:08:52 +0300

    Добавил переход между полями ввода. Настроил поля ввода

---
+++ Grates/NetworkCallInformation.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkCallInformation.swift
+//  Grates
+//
+//  Created by Out East on 15.12.2023.
+//
+
+import Foundation
---
+++ Grates/Registration/Validator.swift
@@ -0,0 +1,8 @@
+//
+//  Validator.swift
+//  Grates
+//
+//  Created by Out East on 22.12.2023.
+//
+
+import Foundation
